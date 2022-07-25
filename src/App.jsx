7897c0b95261fcb47c6f3f2555974b0c968458c3
Einon <einommoraes@gmail.com>
2022-07-25 20:32:17 -0300

    feat: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { Header } from './components/Header';
+import { Header } from './components/Header/Header';
+import { Post } from './Post';
+ 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 function App() {
 
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+          <Sidebar/> 
+          <main>
+            <Post 
+              author="Einon Moraes"
+              content="O einon é muito legal"
+            />
+            <Post 
+              author="Maikinho"
+              content="Pão de queijo"
+            />
+          </main>
+      </div>
     </div>
   )
 }
